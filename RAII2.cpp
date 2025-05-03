91ac21f1966bf5f48701d16dfe09fa0843cbaa96
Artem <70117318+044RoHS@users.noreply.github.com>
2025-05-03 18:18:56 +0300

    Second commit

--- RAII2.cpp
+++ RAII2.cpp
@@ -41,7 +41,7 @@ public:
     if (capacity <= size )
     {
       size_t new_capacity = capacity *2;
-      int* new_data = new int [capacity];
+      int* new_data = new int [new_capacity];
 
       for (int i = 0 ; i < size ; ++i)
       {
